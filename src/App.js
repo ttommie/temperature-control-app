d19273a6bcb8799470d1633cab482fdd9ef29926
Tommie <devtommie@gmail.com>
2022-05-25 13:45:49 -0400

    Temperature Control App
    Create a controlled temperature status using react hooks.
    This is affected with two buttons that allow you to change the
    temperature.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from '../src/style.module.css'
 
 function App() {
+  const [temperature, setTemperature] = useState(68);
+
+  const HandleSubmit = (e) => {
+    if(e.target.id === "pushedUp") {
+      setTemperature(temperature + 1);
+    }
+
+    if (e.target.id === "pushedDown") {
+      setTemperature(temperature - 1);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.centerStyle}>
+      <div className={styles.boxStyle}>
+        <h1 className={styles.temperatureStyle}>{temperature}°F</h1>
+        <div>
+          <button  id="pushedUp" onClick={(e) => HandleSubmit(e)} className={[styles.btnStyle, styles.btnUpStyle].join(" ")} type="button">⮝</button>
+           <button id="pushedDown" onClick={(e) => HandleSubmit(e)} className={[styles.btnStyle, styles.btnDownStyle].join(" ")} type="button">⮟</button>
+        </div>
+      </div>
     </div>
   );
 }
