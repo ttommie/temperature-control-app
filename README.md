6d31342330f18f9c3cd9b6e3acb41a936d47446a
Tommie <devtommie@gmail.com>
2022-06-06 11:30:44 -0400

    Background updates
    Added a color changing background based on temperature input.
    Created readme.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-### Readme Coming Soon
+# Tempature Control App
+
+Control the background based on the temperature specified on the screen.
+
+_note: not for real world use._
+
+## Features
+
+-   Real-time Input & Background Updates.
+-   Live Preview
+-   Mobile Accessible
+
+## Deployment
+
+Clone or Download
+
+```bash
+git clone https://github.com/ttommie/temperature-control-app.git
+```
+
+Install all node modules
+
+```bash
+npm install
+```
+
+Run the react server
+
+```bash
+npm start
+```
+
+## Demo
+
+[Live Demo](https://ttommie.github.io/temperature-control-app/)
--- src/App.js
+++ src/App.js
@@ -3,23 +3,37 @@ import styles from '../src/style.module.css'
 
 function App() {
   const [temperature, setTemperature] = useState(68);
+  const [tempColor, setTempColor ] = useState(styles.neutral)
 
-  const HandleSubmit = (e) => {
-    if(e.target.id === "pushedUp") {
+  const HandleClick = (e) => {
+    if (e.target.id === "tempUp") {
       setTemperature(temperature + 1);
     }
 
-    if (e.target.id === "pushedDown") {
+    if (e.target.id === "tempDown") {
       setTemperature(temperature - 1);
     }
+
+    if (temperature > 70) {
+      setTempColor(styles.hot)
+    } else if (temperature < 65) {
+      setTempColor(styles.cold)
+    } else {
+      setTempColor(styles.neutral)
+    }
   }
+
   return (
-    <div className={styles.centerStyle}>
+    <div className={[styles.centerStyle, tempColor].join(" ")}>
       <div className={styles.boxStyle}>
         <h1 className={styles.temperatureStyle}>{temperature}°F</h1>
         <div>
-          <button  id="pushedUp" onClick={(e) => HandleSubmit(e)} className={[styles.btnStyle, styles.btnUpStyle].join(" ")} type="button">⮝</button>
-           <button id="pushedDown" onClick={(e) => HandleSubmit(e)} className={[styles.btnStyle, styles.btnDownStyle].join(" ")} type="button">⮟</button>
+          <button id="tempUp" onMouseDown={(e) => HandleClick(e)} className={[styles.btnStyle, styles.btnUpStyle].join(" ")} type="button">
+            ⮝
+          </button>
+           <button id="tempDown" onMouseDown={(e) => HandleClick(e)} className={[styles.btnStyle, styles.btnDownStyle].join(" ")} type="button">
+            ⮟
+          </button>
         </div>
       </div>
     </div>
